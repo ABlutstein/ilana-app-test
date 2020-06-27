4b9a81550a7181ad3ff944d564719087327e4882
Work H <aaronblutstein@Work-H.local>
2020-06-27 18:20:18 +0100

    Updates

---
+++ precache-manifest.eb0d6d8d126deace8667259985cd0b4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910787ee717f19a28b4f27057fa21473",
+    "url": "/ABlutstein/ilana-app-test.git/index.html"
+  },
+  {
+    "revision": "dff50d6855d80ceff454",
+    "url": "/ABlutstein/ilana-app-test.git/static/css/main.ce64581c.chunk.css"
+  },
+  {
+    "revision": "d97a0591415f9beee6bb",
+    "url": "/ABlutstein/ilana-app-test.git/static/js/2.3e0c7f09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ABlutstein/ilana-app-test.git/static/js/2.3e0c7f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff50d6855d80ceff454",
+    "url": "/ABlutstein/ilana-app-test.git/static/js/main.70933f5d.chunk.js"
+  },
+  {
+    "revision": "97a46b4bfb0488b05799",
+    "url": "/ABlutstein/ilana-app-test.git/static/js/runtime-main.f48c4ab3.js"
+  },
+  {
+    "revision": "43ee340b7f94cb2df2723af997f02aa3",
+    "url": "/ABlutstein/ilana-app-test.git/static/media/RainbowPic.43ee340b.jpg"
+  }
+]);
